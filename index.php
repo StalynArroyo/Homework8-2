7665fafbfc6658e9a4b6f51092486fbfe12f3887
Stalyn Arroyo <sma47@njit.edu>
2017-05-11 22:46:28 -0400

    Finalized HW8-2

--- index.php
+++ index.php
@@ -11,6 +11,8 @@ $score_total = 0;
 $score_average = 0;
 $max_rolls = 0;
 $average_rolls = 0;
+$score_total_f = '';
+$score_average_f = '';
 
 //take action based on variable in POST array
 $action = filter_input(INPUT_POST, 'action');
@@ -20,21 +22,26 @@ switch ($action) {
 
         // validate the scores
         // TODO: Convert this if statement to a for loop
-        if (empty($scores[0]) ||
-            empty($scores[1]) ||
-            empty($scores[2]) ||
-            !is_numeric($scores[0]) ||
-            !is_numeric($scores[1]) ||
-            !is_numeric($scores[2])) {
-                $scores_string = 'You must enter three valid numbers for scores.';
-                break;
-        }
+         $is_valid = true;
+	 for ($i = 0; $i < count($scores); $i++) 
+	 {
+	 	if (empty($scores[$i]) || !is_numeric($scores[$i])) 
+		{
+			$scores_string = 'You must enter three valid numbers for scores.';
+			$is_valid = false;
+			break;
+		}
+	 }
+	if (!$is_valid) 
+	    {break;}
 
-        // process the scores
-        // TODO: Add code that calculates the score total
-        $scores_string = '';
-        foreach ($scores as $s) {
+        // process the score
+        // TODO: Add code that calculates the score tota
+        $scores_total = 0;
+        foreach ($scores as $s)
+	{
             $scores_string .= $s . '|';
+	    $score_total += $s;
         }
         $scores_string = substr($scores_string, 0, strlen($scores_string)-1);
 
@@ -51,20 +58,21 @@ switch ($action) {
                 FILTER_VALIDATE_INT);
 
         $total = 0;
-        $count = 0;
-        $max_rolls = -INF;
+	$max_rolls = -INF;
 
         // TODO: convert this while loop to a for loop
-        while ($count < 10000) {
-            $rolls = 1;
-            while (mt_rand(1, 6) != 6) {
-                $rolls++;
-            }
-            $total += $rolls;
-            $count++;
-            $max_rolls = max($rolls, $max_rolls);
+        for ($count = 0; $count < 10000; $count++)
+	{
+		$rolls = 1;
+	        while (mt_rand(1, 6) != $number_to_roll) 
+		{
+            		$rolls++;
+            	} 
+                $total += $rolls;
+                $max_rolls = max($rolls, $max_rolls);
         }
-        $average_rolls = $total / $count;
+       
+       $average_rolls = $total / $count;
 
         break;
 }
